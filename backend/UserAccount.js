444ec56ff825034656f539193eda70395570c94d
David Zhang <davidzhangzuhao@gmail.com>
2023-10-25 19:58:09 -0700

    Added Backend server with only UserAccount and UserStore

---
+++ backend/UserAccount.js
@@ -0,0 +1,181 @@
+
+class UserAccount {
+    constructor(io, userStore) {
+        this.io = io;
+        this.userStore = userStore;
+    }
+
+    async retrieveAccount(socket, userId) {
+        const user = await this.userStore.getUser(userId);
+        if (!user) {
+            socket.emit('userError', "User not found.");
+            return;
+        }
+
+        socket.emit('userAccountDetails', user);
+    }
+
+    async updateAccount(socket, userInfo) {
+
+        const updatedUser = await this.userStore.updateUser(userInfo.userId, userInfo);
+        if (!updatedUser) {
+            socket.emit('userError', "Update failed or user not found.");
+            return;
+        }
+        socket.emit('accountUpdated', updatedUser);
+    }
+
+
+    async createAccount(socket, userInfo) {
+        const newUser = await this.userStore.addUser(userInfo);
+        if (!newUser) {
+            socket.emit('userError', "Create User Failed.");
+            return;
+        }
+        socket.emit('accountCreated', newUser);
+    }
+
+    async updateName(socket, userId, name) {
+        const user = await this.userStore.getUser(userId);
+        if (!user) {
+            socket.emit('userError', "User not found.");
+            return;
+        }
+        
+        user.username = name;
+        console.log(user);
+        const updatedUser = await this.userStore.updateUser(userId, user);
+        console.log(updatedUser)
+        if (!updatedUser) {
+            socket.emit('userError', "Update username failed.");
+            return;
+        }
+        
+        socket.emit('accountUpdated', updatedUser);
+    }
+
+
+    
+    async updateAdminStatus(socket, userId, isAdmin) {
+        const user = await this.userStore.getUser(userId);
+        if (!user) {
+            socket.emit('userError', "User not found.");
+            return;
+        }
+        
+        user.isAdmin = isAdmin;
+        const updatedUser = await this.userStore.updateUser(userId, user);
+        
+        if (!updatedUser) {
+            socket.emit('userError', "Assign Admin Status failed.");
+            return;
+        }
+        
+        socket.emit('accountUpdated', updatedUser);
+    }
+
+        
+    async updateChatBanned(socket, userId, isChatBanned) {
+        const user = await this.userStore.getUser(userId);
+        if (!user) {
+            socket.emit('userError', "User not found.");
+            return;
+        }
+        
+        user.isChatBanned = isChatBanned;
+        const updatedUser = await this.userStore.updateUser(userId, user);
+        
+        if (!updatedUser) {
+            socket.emit('userError', "Assign ChatBanned Status failed.");
+            return;
+        }
+        
+        socket.emit('accountUpdated', updatedUser);
+    }
+
+    async updateLastRedemptionDate(socket, userId, date){
+        const user = await this.userStore.getUser(userId);
+        if (!user) {
+            socket.emit('userError', "User not found.");
+            return;
+        }
+        
+        user.lastRedemptionDate = date;
+        const updatedUser = await this.userStore.updateUser(userId, user);
+        
+        if (!updatedUser) {
+            socket.emit('userError', "Update lastRedemptionDate failed.");
+            return;
+        }
+        
+        socket.emit('accountUpdated', updatedUser);
+    }
+
+    async deposit(socket, userId, amount) {
+        const user = await this.userStore.getUser(userId);
+        if (!user) {
+            socket.emit('userError', "User not found.");
+            return;
+        }
+        
+        user.balance += amount;
+        const updatedUser = await this.userStore.updateUser(userId, user);
+        
+        if (!updatedUser) {
+            socket.emit('userError', "Deposit failed.");
+            return;
+        }
+        
+        socket.emit('balanceUpdate', updatedUser.balance);
+    }
+
+    async withdraw(socket, userId, amount) {
+        const user = await this.userStore.getUser(userId);
+        if (!user) {
+            socket.emit('userError', "User not found.");
+            return;
+        }
+        
+        user.balance -= amount;
+        const updatedUser = await this.userStore.updateUser(userId, user);
+        
+        if (!updatedUser) {
+            socket.emit('userError', "Withdraw failed.");
+            return;
+        }
+        
+        socket.emit('balanceUpdate', updatedUser.balance);
+    }
+
+    async deleteUser(socket, userId) {
+        const deletedCount = await this.userStore.deleteUser(userId);
+        if (deletedCount) {
+            socket.emit('userDeleted', `User with ID ${userId} deleted successfully.`);
+        } else {
+            socket.emit('userError', `Failed to delete user with ID ${userId}.`);
+        }
+    }
+
+    async deleteAllUsers(socket){
+        const result = await this.userStore.deleteAllUsers();
+        if (result) {
+            socket.emit('allUsersDeleted');
+        } else {
+            socket.emit('userError', "Failed to delete users.");
+        }
+    }
+
+    async checkUserisInDB(socket, userId){
+        const user = await this.userStore.getUser(userId);
+        if (!user) {
+            socket.emit('UserFound', null);
+            return;
+        }else{
+            socket.emit("UserFound", user);
+        }
+
+    }
+    // More functionalities as required...
+}
+
+module.exports = UserAccount;
---
+++ backend/server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+app.use(express.static(__dirname));
+const server = http.createServer(app);
+const io = socketIo(server);
+const cors = require('cors');
+app.use(cors());
+
+const UserStore = require('./UserStore');
+const UserAccount = require('./UserAccount'); // Assuming you also export the UserAccount class
+
+const MONGO_CONNECTION_STRING = 'mongodb://localhost:27017'; // Adjust to your MongoDB connection string
+const DATABASE_NAME = 'UserDB'; // Name of the database
+
+const userStore = new UserStore(MONGO_CONNECTION_STRING, DATABASE_NAME);
+userStore.connect();
+
+const userAccount = new UserAccount(io, userStore);
+
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('retrieveAccount', (userId) => {
+        userAccount.retrieveAccount(socket, userId);
+    });
+
+    socket.on('updateAccount', (userInfo) => {
+        userAccount.updateAccount(socket, userInfo);
+    });
+    
+    socket.on('createAccount', (userInfo) => {
+        userAccount.createAccount(socket, userInfo);
+    });
+
+    socket.on('updateName', (userId, newname) => {
+        userAccount.updateName(socket, userId, newname);
+    });
+
+    socket.on('updateAdminStatus', (userID, AdminStatus) => {
+        userAccount.updateAdminStatus(socket, userID, AdminStatus);
+    });
+
+    socket.on('updateChatBanned', (userID, ChatBannedStatus) => {
+        userAccount.updateChatBanned(socket, userID, ChatBannedStatus);
+    });
+
+    socket.on('updateLastRedemptionDate', (userID, rDate) => {
+        userAccount.updateLastRedemptionDate(socket, userID, rDate);
+    });
+
+    socket.on('deposit', (userId, amount) => {
+        userAccount.deposit(socket, userId, amount);
+    });
+
+    socket.on('withdraw', (userId, amount) => {
+        userAccount.withdraw(socket, userId, amount);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+
+    socket.on('deleteUser', async (userId) => {
+       userAccount.deleteUser(socket, userId);
+    });
+    
+    socket.on('deleteAllUsers', async () => {
+       userAccount.deleteAllUsers(socket);
+    });
+
+    
+
+});
+
+server.listen(8081, () => {
+    console.log('listen to port 8081');
+})
+
