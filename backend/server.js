d6e7f1e72c45713190ffcefa66337bd8b0676e2e
seriouscoding123 <dingwen1909@163.com>
2023-10-27 21:50:55 -0700

    combined backend

--- backend/server.js
+++ backend/server.js
@@ -11,15 +11,28 @@ app.use(cors());
 
 const UserStore = require('./UserStore');
 const UserAccount = require('./UserAccount'); // Assuming you also export the UserAccount class
+const GameLobby = require('./GameLobby');
+const GameLobbyStore = require('./GameLobbyStore');
+const LiveChat = require('./LiveChat');
 
 const MONGO_CONNECTION_STRING = 'mongodb://localhost:27017'; // Adjust to your MongoDB connection string
-const DATABASE_NAME = 'UserDB'; // Name of the database
+const USERDB_NAME = 'UserDB'; // Name of the database
+const LOBBYDB_NAME = 'gameLobbyDB'
 
-const userStore = new UserStore(MONGO_CONNECTION_STRING, DATABASE_NAME);
-userStore.connect();
+const userStore = new UserStore(MONGO_CONNECTION_STRING, USERDB_NAME);
+
+(async () => {
+    await userStore.connect();
+})();
 
 const userAccount = new UserAccount(io, userStore);
 
+const gameLobbyStore = new GameLobbyStore(MONGO_CONNECTION_STRING, LOBBYDB_NAME);
+const gameLobbies = {};
+
+(async () => {
+    await gameLobbyStore.init();
+})();
 
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
@@ -64,9 +77,9 @@ io.on('connection', (socket) => {
         userAccount.withdraw(socket, userId, amount);
     });
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
-    });
+    // socket.on('disconnect', () => {
+    //     console.log('User disconnected:', socket.id);
+    // });
 
     socket.on('deleteUser', async (userId) => {
        userAccount.deleteUser(socket, userId);
@@ -76,11 +89,75 @@ io.on('connection', (socket) => {
        userAccount.deleteAllUsers(socket);
     });
 
-    
+    socket.on('createLobby', async (roomName, gameType, maxPlayers, userName) => {
+        console.log("Lobby created");
+        if(!gameLobbies[roomName]) {
+            // Set GameManager = null for now
+            const lobby = new GameLobby(roomName, gameType, maxPlayers, null, gameLobbyStore, io);
+            await lobby.init(roomName, gameType, maxPlayers);
+            gameLobbies[roomName] = lobby;
+            // Set bet = 0 initially
+            var success = await gameLobbies[roomName].addPlayer(userName, 0, socket);
+            if(success === true) {
+                gameLobbies[roomName].registerSocketEvents(socket);
+            }
+        }
+        else {
+            socket.emit('roomAlreadyExist', "Room already exist");
+        }
+
+    });
+
+    socket.on('joinLobby', async (roomName, userName) => {
+        console.log("User joined");
+        if(gameLobbies[roomName]) {
+            // Set bet = 0 initially
+            var success = await gameLobbies[roomName].addPlayer(userName, 0, socket);
+            if(success === true) {
+                gameLobbies[roomName].registerSocketEvents(socket);
+            }
+        }
+        else {
+            socket.emit('roomDoesNot', "Room does not exist");
+        }
+    });
+
+    socket.on('getAllLobby', async () => {
+        var myLobbies = await gameLobbyStore.getAllLobby();
+        socket.emit('AllLobby', myLobbies);
+    });
+
+    socket.on('setBet', async(roomName, userName, bet) => {
+        await gameLobbies[roomName].setPlayerBet(roomName, userName, bet);
+    })
+
+    socket.on('leaveLobby', () => {
+        console.log("User left lobby");
+        for(let roomName in gameLobbies) {
+            for(let userName in gameLobbies[roomName].players) {
+                if(gameLobbies[roomName].players[userName].socketId === socket.id) {
+                    gameLobbies[roomName].removePlayer(userName);
+                    break;
+                }
+            }
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log("User disconnected");
+        for(let roomName in gameLobbies) {
+            for(let userName in gameLobbies[roomName].players) {
+                if(gameLobbies[roomName].players[userName].socketId === socket.id) {
+                    gameLobbies[roomName].removePlayer(userName);
+                    break;
+                }
+            }
+        }
+    });
 
 });
 
-server.listen(8081, () => {
-    console.log('listen to port 8081');
+server.listen(3000, () => {
+    console.log('listen to port 3000');
 })
 
